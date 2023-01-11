a3bf5f4f2852918b2ff457bbf2f7dd1344ba11da
radio-satellites <114111180+radio-satellites@users.noreply.github.com>
2023-01-11 11:06:23 -0500

    Oops..... wrong file!!

--- find_from_date.py
+++ find_from_date.py
@@ -7,29 +7,22 @@ import numpy as np
 from fastkml import kml
 from shapely.geometry import Point, LineString, Polygon
 
+positions = []
+
 # load tle
 tle = open('tle.txt', 'r')
 tle_lines = tle.readlines()
 tle.close()
 
-passes_out = open("passes.txt", 'w')
-
 # create satellite object
 satellite = ephem.readtle(tle_lines[0], tle_lines[1], tle_lines[2])
 
-start_dt = dt.datetime.today()
-intervall = dt.timedelta(minutes=1)
+t = dt.datetime(2023, 1, 4, 9, 48, 30)
 
-timelist = []
-for i in range(1000):
-  timelist.append(start_dt + i * intervall)
-
-positions = []
-for t in timelist:
-  satellite.compute(t)
-  positions.append((satellite.sublong / ephem.degree,
-                    satellite.sublat / ephem.degree, satellite.elevation))
-  #print(satellite.elevation)
+satellite.compute(t)
+positions.append((satellite.sublong / ephem.degree,
+                  satellite.sublat / ephem.degree, satellite.elevation))
+#print(satellite.elevation)
 
 #print(positions)
 """
@@ -47,33 +40,12 @@ satellite_obs = ephem.Observer()  #hacky
 counter = 0
 pass_now = False
 for i in positions:
+  #print(len(positions))
   satellite_obs.lon = i[0]
   satellite_obs.lat = i[1]
   satellite_obs.elevation = i[2]
-  satellite_obs.date = timelist[counter]
+  satellite_obs.date = t
   m = ephem.Moon()
   m.compute(satellite_obs)
-  if m.alt > 0 and pass_now == False:
-    print("NEW PASS:")
-    print("START TIME: " + str(timelist[counter]))
-    print("EL AT START" + str((m.alt / ephem.degree)))
-    print("AZ AT START" + str((m.az / ephem.degree)))
-    pass_now = True
-    passes_out.write("NEW PASS:")
-    passes_out.write("START TIME: " + str(timelist[counter]) + "\n")
-    passes_out.write("EL AT START: " + str((m.alt / ephem.degree)) + "\n")
-    passes_out.write("AZ AT START: " + str((m.az / ephem.degree)) + "\n")
-  if m.alt < 0 and pass_now == True:
-    print("END OF PASS:")
-    print("END TIME: " + str(timelist[counter]))
-    print("EL AT END" + str((m.alt / ephem.degree)))
-    print("AZ AT END" + str((m.az / ephem.degree)))
-    passes_out.write("END OF PASS:\n")
-    passes_out.write("END TIME: " + str(timelist[counter]) + "\n")
-    passes_out.write("EL AT END" + str((m.alt / ephem.degree)) + "\n")
-    passes_out.write("AZ AT END" + str((m.az / ephem.degree)) + "\n")
-
-    pass_now = False
-  counter = counter + 1
-
-passes_out.close()
+  if m.alt > 0:
+    print("Moon was above the horizon.")
